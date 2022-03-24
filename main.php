f68707f3153d26e306d4e075cc714bb807902343
Alexander Pomazkin <kjkdsk@yandex.ru>
2022-03-24 20:04:13 +0700

    practica 10

--- main.php
+++ main.php
@@ -1,27 +1,40 @@
-<?php
-mb_internal_encoding("UTF-8");
-if (!empty($_REQUEST['text'])) {
-    $str = $_REQUEST['text'];
-    $strLen = mb_strlen($str);
-    $valuesCount = array_count_values(str_split($str));
-    foreach ($valuesCount as $key => $item) {
-        $x = 100 / $strLen * $item;
-        echo '"'.$key.'" - "'.round($x, 2).'"<br>';
-    }
+ <?php
+$month = [1 => 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+$week = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+if (isset($_REQUEST)) {
+    $day = $_REQUEST['day'];
+    $mon = array_search($_REQUEST['month'], $month);
+    $year = $_REQUEST['year'];
+    $dayOfWeek = $week[date('w', mktime(0, 0, 0, $mon, $day, $year))];
+    echo $dayOfWeek;
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+ <body>
     <form action="main.php" method="post">
-  <textarea name="text" placeholder="">hello my friend!
-</textarea>
+    <select name="day">
+        <?php for ($i = 1; $i <= 31; $i++): ?>
+            <option><?=$i;?></option>
+        <?php endfor;?>
+    </select>
+    <select name="month">
+        <?php foreach ($month as $item): ?>
+            <option><?=$item;?></option>
+        <?php endforeach;?>
+    </select>
+    <select name="year">
+        <?php for ($i = 1990; $i <= 2025; $i++): ?>
+            <option><?=$i;?></option>
+        <?php endfor;?>
+    </select>
     <input type="submit">
 </form>
-</body>
-</html>
+     
+ </body>
+ </html>
