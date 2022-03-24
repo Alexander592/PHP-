c7decb669823029ec9375946ac32e5507db3bbf8
Alexander Pomazkin <kjkdsk@yandex.ru>
2022-03-24 19:08:46 +0700

    practica 4

--- main.php
+++ main.php
@@ -1,10 +1,11 @@
 <?php
     $num = $_REQUEST[1];
+    $num2 = $_REQUEST[2];
     for($i = 1; $i <= $num; $i++) {
-        if($num % $i == 0) {
+        if(($num % $i == 0) and ($num2 % $i == 0)) {
             $arr[] = $i;
         }}
-        echo "Делители числа ".$num.": ".implode(', ', $arr);
+        echo "Делители чисел ".$num." и ".$num2.": ".implode(', ', $arr);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,6 +17,7 @@
 <body>
     <form action="main.php" method="GET">
 <input type="text" name="1"><br><br>
+<input type="text" name="2"><br><br>
 <input type="submit" name="">
 </form>
 </body>
