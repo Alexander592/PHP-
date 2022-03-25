1d542cefa8f49cd285e1553d3298853d946e5cce
Alexander <olexander.pomazkin@yandex.ru>
2022-03-25 13:26:35 +0700

    forma

--- s/index.php
+++ s/index.php
@@ -1,9 +1,10 @@
 <?php
+
 	session_start();
-	if(!isset($_SESSION['counter']))
-		$_SESSION['counter']=0;
-	else
-		$_SESSION['counter']++;
+	if(!isset($_SESSION['user_name']))
+		header('Location: login.php');
+	echo $name;
+
 
 ?>
 
@@ -14,6 +15,17 @@
 	<title>Document</title>
 </head>
 <body>
+	<h1>Главная страница</h1>
 	<?php echo $_SESSION['counter']; ?>
+	<h2><?php echo "Hello " ?>
+		<a href="forma.php"><?php echo $_SESSION['user_name']; ?></a>
+	</h2>
+	<h1>Форма:</h1>
+	<p>
+		<span>Имя: </span><?php echo $_SESSION['namee']; ?>
+	</p>
+	<p>
+		<span>Фамилия: </span><?php echo $_SESSION['snamee']; ?>
+	</p>
 </body>
 </html>
