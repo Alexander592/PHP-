8b162b5d5ce48b6d6c379ff2a81ea9597cdbbac7
Alexander Pomazkin <kjkdsk@yandex.ru>
2022-04-06 11:39:45 +0700

    spisok students

--- tt/index.php
+++ tt/index.php
@@ -1,12 +1,13 @@
- <?php
+<?php
 header('Content-Type: text/html; charset=utf-8');
 require_once 'student.php';
 require_once 'group.php';
-
 $g1 = new Group(1, 'И-01');
 $g2 = new Group(1, 'И-02');
 $g3 = new Group(1, 'И-03');
 
-$s1 = new Student('Помазкин','Александр','Александрович', $g3);
-$s1->display();
+$s1 = new Student('Винокуров','Михаил','Игоревич', $g3);
+$s2 = new Student('Бобровский','Константин','Михайлович', $g3);
+$s2 = new Student('Помазкин','Александр','Александрович', $g3);
+$g3->display();
 ?> 
