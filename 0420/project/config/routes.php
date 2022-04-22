b8fc55728f9b0226f6c70c500c2e00350fadb0b9
Alexander <olexander.pomazkin@yandex.ru>
2022-04-22 11:01:41 +0700

    Глава 96

--- 0420/project/config/routes.php
+++ 0420/project/config/routes.php
@@ -2,11 +2,8 @@
 	use \Core\Route;
 
 	return [
-		new Route('/nums/:n1/:n2/:n3/', 'num', 'sum'),
-		new Route('/user/:id/', 'user', 'show'),
-		new Route('/user/:id/:key/', 'user', 'info'),
-		new Route('/user/all', 'user', 'all'),
-		new Route('/users/first/:n/', 'user', 'first')
+		new Route('/product/:n/', 'product', 'show'),
+		new Route('/product/all', 'product', 'all')
 	];
 
 ?> 
