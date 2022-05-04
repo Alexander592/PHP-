1a90e6334201ac78d3c5ddfe6be8246608f5cbcb
Alexander <olexander.pomazkin@yandex.ru>
2022-05-04 13:26:37 +0700

    глава 4

--- wordpress/wp-content/themes/mytheme/index.php
+++ wordpress/wp-content/themes/mytheme/index.php
@@ -3,10 +3,7 @@
 
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<title>
-<?php echo wp_get_document_title(); ?>
-</title>
-
+echo '<title>'.pozitiv  .'</title>';
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
 
 <?php wp_head(); ?>
@@ -27,11 +24,17 @@ while ( have_posts() ){
 the_post();
 
 echo '<h2><a href="'. get_permalink() .'">'. get_the_title() .'</a></h2>';
+echo '<div class="teaser">'. apply_filters( 'the_content', get_the_content('подробнее')) .'</div>';
 
 echo get_the_excerpt();
 echo '<h2><a href="'. get_permalink() .'">подробнее</a></h2>';
 the_title();
 the_content();
+the_author();
+the_date('d.m.Y h:m:i');
+echo get_the_category_list(',');
+echo get_the_tag_list('', ' | ');
+echo get_the_post_thumbnail();
 }
 }
 // елси записей не найдено
@@ -41,10 +44,11 @@ echo ' <p>Записей нет...</p>';
 ?>
 </div>
 
-<footer class="footer">
-<?php echo date('Y') ?> © Я и компания моя
-</footer>
 
+<footer class="footer">
+<?php echo date('Y') ?> © Я и компания моя  cамые самые 
+<?php bloginfo('description'); ?>
+</footer>
 <?php wp_footer(); ?>
 </body>
 
